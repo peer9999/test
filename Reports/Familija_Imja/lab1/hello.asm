76e55ed76835f20bad22eb3f0d4610ece41f5e60
I am <peer9999@gmail.com>
2021-02-23 11:46:25 +0300

    add

---
+++ Reports/Familija_Imja/lab1/hello.asm
@@ -0,0 +1,13 @@
+section .text
+    global main ;must be declared forlinker (ld)
+main:           ;tells linker entry point
+    mov edx,len ;message length
+    mov ecx,msg ;message to write
+    mov ebx,1   ;file descriptor (stdout)
+    mov eax,4   ;system call number (sys_write)
+    int 0x80    ;call kernel
+    mov eax,1   ;system call number (sys_exit)
+    int 0x80    ;call kernel
+section .data
+    msg db 'Hello, world!',0xa      ;ourdear string
+    len equ $ -msg                  ;length of ourdear string
